31de057b18226f77d48d480e73ffdda6f8373b0b
Zeeshan Anjum <zeerox92@gmail.com>
2021-04-27 11:37:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Zeeshannn.
+          Edit <code>src/App.js</code> and save to reload Anjum, Zeeshan.
         </p>
         <a
           className="App-link"
