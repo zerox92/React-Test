da9717bd3177cb1c83bb288854407d265219b180
Zeeshan Anjum <anjuz@amazon.com>
2021-05-27 16:22:43 +0100

    Test update.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Anjum, Zeeshan.
+          Edit <code>src/App.js</code> and save to reload the application.
         </p>
         <a
           className="App-link"
