ab069623a11710f2d87587e5c1a5f24747eb90ed
Zeeshan Anjum <anjuz@amazon.com>
2021-05-27 16:56:58 +0100

    adsads

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload the application crith prthehhhsds.
+          Edit <code>src/App.js</code> and save to reload the application llllllllll.
         </p>
         <a
           className="App-link"
